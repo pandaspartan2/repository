3cc89bf5fd2ac9be05514f1fbb2f3a154871e93e
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-09-14 10:08:21 -0500

    Add files via upload

---
+++ repository.swift/repository.swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  repository.swift
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ repository.swift/repository.swift/repository_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  repository_swiftApp.swift
+//  repository.swift
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct repository_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
